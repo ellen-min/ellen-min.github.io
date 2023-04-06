781b21f8dfa88f414d1010749484c22d0b8fee9a
ellen-min <emin@caltech.edu>
2023-04-05 21:59:03 -0700

    committing changes

---
+++ index-interactions.js
@@ -0,0 +1,22 @@
+/**
+* @author Ellen Min
+* Last updated: 04-05-2023
+* This is the javascript page for index.html for my personal website.
+*/
+
+(function () {
+  "use strict";
+
+  function init() {
+    id("myname").addEventListener("mouseout", toggleName);
+    id("mynamehidden").addEventListener("mouseover", toggleName);
+  }
+
+  function toggleName() {
+    id("myname").classList.toggle("hidden");
+    id("mynamehidden").classList.toggle("hidden");
+  }
+
+  init();
+
+})();
---
+++ resume-interactions.js
@@ -0,0 +1,38 @@
+/**
+* @author Ellen Min
+* Last updated: 04-05-2023
+* This is the javascript page for resume.html for my personal website.
+*/
+
+(function () {
+  "use strict";
+
+  function init() {
+    let allExperiences = qsa("div.experience");
+    for (let i = 0; i < allExperiences.length; i++) {
+      allExperiences[i].addEventListener("mouseover", highlightExp);
+      allExperiences[i].addEventListener("mouseout", highlightExp);
+    }
+  }
+
+  function highlightExp() {
+    let companyName = this.querySelector("h2");
+    companyName.classList.toggle("highlighted");
+
+    let descriptions = this.querySelectorAll("p");
+    for (let i = 0; i < descriptions.length; i++) {
+      let currDesc = descriptions[i];
+      if (currDesc.querySelectorAll("a").length == 0) {
+        currDesc.classList.toggle("highlightedDescrip");
+      } else {
+        currDesc.classList.toggle("workex");
+      }
+    }
+
+    this.querySelector("h3").classList.toggle("highlightedLoc");
+    this.querySelector("h4").classList.toggle("highlightedLoc"); // terrible style fix later
+    }
+
+  init();
+
+})();
